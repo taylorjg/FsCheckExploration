ffda8da95cbacbb481e5c2c5ba5df09d4d515442
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-08-11 15:00:27 +0100

    Initial commit

---
+++ PropertyTests/ReverserTests.cs
@@ -0,0 +1,31 @@
+﻿using Code;
+using FsCheck.Fluent;
+using NUnit.Framework;
+
+namespace PropertyTests
+{
+    [TestFixture]
+    internal class ReverserTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("a")]
+        [TestCase("ab")]
+        [TestCase("abc")]
+        [TestCase("abcd")]
+        [TestCase("abcde")]
+        public void StringReversedAndThenReversedAgainIsSameAsOriginalString(string s)
+        {
+            Assert.That(s.Reverse().Reverse(), Is.EqualTo(s));
+        }
+
+        [FsCheck.NUnit.Property]
+        public void TestUsingFsCheck()
+        {
+            var gen = Any.OfType<string>();
+            Spec
+                .For(gen, s => s.Reverse().Reverse() == s)
+                .QuickCheckThrowOnFailure();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+Add the following NuGet packages:
+
+* [FsCheck](http://www.nuget.org/packages/FsCheck/)
+* [FsCheck.NUnit](http://www.nuget.org/packages/FsCheck.Nunit/)
+
+If using VS2012, the following *pre-release* NuGet package is also needed
+in order to obtain FSharp.Core.dll v 4.3.1.0
+(FsCheck is built against F# 3.1 which only seems to be supported on VS2013)
+
+* [FSharp.Core.Microsoft.Signed](http://www.nuget.org/packages/FSharp.Core.Microsoft.Signed/0.0.1-beta1)
+
