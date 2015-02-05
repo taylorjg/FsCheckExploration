485bffb2ef3eff8723ee1ebef6b16109af629cbd
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-05 21:42:01 +0000

    Finally figured out how to use DontShrink

---
+++ FsCheckExploratoryTests/RegularTests/DontShrinkTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FsCheck;
+using FsCheck.Fluent;
+using Microsoft.FSharp.Core;
+using NUnit.Framework;
+
+namespace FsCheckExploratoryTests.RegularTests
+{
+    using Property = Gen<Rose<Result>>;
+
+    [TestFixture]
+    public class DontShrinkTests
+    {
+        private static readonly Config Config = Config.VerboseThrowOnFailure;
+
+        [Test]
+        public void Test()
+        {
+            var arb = Arb.Default.DontShrink<IList<int>>();
+            var body = FSharpFunc<DontShrink<IList<int>>, bool>.FromConverter(dsxs =>
+            {
+                var xs = dsxs.Item;
+                return xs.Reverse().Reverse().SequenceEqual(xs);
+            });
+            Check.One(Config, Prop.forAll(arb, body));
+        }
+
+        [FsCheck.NUnit.Property(Verbose = true)]
+        public Property Property(DontShrink<IList<int>> dsxsParam)
+        {
+            var generator = Any.Value(dsxsParam).Select(dsxs => dsxs.Item);
+            Func<IList<int>, bool> assertion = xs =>
+                xs.Reverse().Reverse().SequenceEqual(xs);
+            return Spec.For(generator, assertion).Build();
+        }
+    }
+}
